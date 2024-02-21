8a2c75166a29bb05e1521aeb25cb28840397aaed
Magali18 <pereyrahmagali@gmail.com>
2024-02-20 21:17:22 -0300

    style

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import PrimarySearchAppBar from "./Components/barraDeTareas/PrimarySearchAppBar";
+import Card from "./Components/Card/Card";
+import { Box } from "@mui/system";
+import imagen from "./image/417134192_7035273036527283_1575827752176715560_n.jpg";
+import backImage from "./image/9666d6952c150d93bceea187aa91848e.jpg";
+import Typography from "@mui/material/Typography";
+
 
 function App() {
+  let imagenBackground = `url(${imagen})`;
+  let imagenBackground1 = `url(${backImage})`;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PrimarySearchAppBar />
+      <Box
+         sx={{
+          position: "relative",
+          borderRadius: "10px",
+          height: "25vh",
+          overflow: "hidden",
+          "&::before": {
+            content: '""',
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            background: imagenBackground,
+            backgroundSize:"contain",
+            zIndex: -1,
+          }
+        }}
+      >
+          <div
+          style={{
+            position: "absolute",
+            top:'10%',
+            left: '10%',
+            right: '10%',
+            bottom: '10%',
+            backgroundImage: imagenBackground1,
+            backgroundSize: "contain",
+            filter: "blur(5px)", // Ajusta el valor de blur según tus necesidades
+            zIndex: -1,
+          }}
+        ></div>
+        
+       <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              fontWeight: 'bold',
+              fontSize: 40,
+              fontFamily: 'Protest Strike',
+              position: "relative",
+              zIndex: 1,
+              color: "#25D366",
+            
+    
+            }}
+          >
+            ESTILoSMART
+          </Typography>
+          </Box>
+      
+       <Card />
     </div>
   );
 }
