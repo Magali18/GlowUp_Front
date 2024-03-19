b2141f46fa376671ca283bd63b279f1e99ac2492
Magali Pereyra <98051334+Magali18@users.noreply.github.com>
2024-03-19 11:51:45 -0300

    update form ainstalar taildwin

--- client/src/Components/NewProduct/NewProduct.jsx
+++ client/src/Components/NewProduct/NewProduct.jsx
@@ -1,10 +1,14 @@
 import Grid from "@mui/material/Grid"
 import "./NewProduct.css"
+import Formulario from "./Formulario";
+
 const NewProduct = ()=>{
     return(
 <div className="fondo">
     <Grid container>
-        <Grid item xs={12} sm={6} className="bordes" style={{height:'100vh'}}></Grid>
+        <Grid item xs={12} sm={6} className="bordes" style={{height:'100vh'}}>
+        <Formulario/>
+        </Grid>
         <Grid item xs={12}  sm={6} className="bordes" style={{height:'100vh'}}></Grid>
 
     </Grid>
