b9e8ef6d8e55b786426b56766161455d3a82bd7e
Magali Pereyra <98051334+Magali18@users.noreply.github.com>
2024-03-27 11:11:55 -0300

    Update Formulario validate

--- client/src/Components/NewProduct/Formulario.jsx
+++ client/src/Components/NewProduct/Formulario.jsx
@@ -5,21 +5,27 @@ const Formulario = () => {
 
   const [input, setInput] = useState({
     name: "",
-    cover_letter:"",
-    price:'$'
+    cover_letter: "",
+    price: '$'
   });
-  console.log(input)
+  
 
-const handleSubmit = (e)=>{
-  e.preventDefault()
-  console.log('hola')
-}
-const handleChange= (e)=>{
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (!input.name || !input.price || !input.cover_letter) {
+      alert('Completar los campos')
+
+    } else { alert('Formulario enviado con exito') }
+
+  }
+
+  const handleChange = (e) => {
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
-}
+  }
   return (
 
     <div className="flex flex-col items-center justify-center h-screen">
@@ -58,10 +64,11 @@ const handleChange= (e)=>{
           ></textarea>
 
           <input
+            
             type="file"
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
             placeholder="Resume"
-            
+
           />
           <button type="submit" className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150">Apply</button>
         </form>
